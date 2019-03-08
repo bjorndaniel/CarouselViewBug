63f775922d94032acf6716abd8b2e9f92e323ceb
Daniel Sandberg <daniel.sandberg@elicit.se>
2019-03-08 10:17:42 +0100

    Added example solution

---
+++ CarouselViewBug/CarouselViewBug/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CarouselViewBug
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<User> Users { get; set; }
+        public MainPageViewModel()
+        {
+            Users = new ObservableCollection<User>(Enumerable.Range(1, 10).Select(_ =>
+                 new User
+                 {
+                     Id = _,
+                     Name = $"User_{_}"
+                 }
+             ));
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
